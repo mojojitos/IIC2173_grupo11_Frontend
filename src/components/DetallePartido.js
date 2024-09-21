841f31c2da0cb40a18f4f3febaed1b6eac145d62
mojojitos <majobarra.2000@uc.cl>
2024-09-21 01:00:23 -0300

    componentes basicos añadidos

---
+++ src/components/DetallePartido.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '.DetallePartido.css';
+
+const Partidos = () => {
+    return <div>Info de partido seleccionado</div>;
+};
+
+export default Partidos;
---
+++ src/components/Login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return <div>Formulario de Login</div>;
+};
+export default Login;
