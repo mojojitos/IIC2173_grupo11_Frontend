94fa74ce4f62f7402c3e3a47fc8f2dbf9cf88c6d
FELIPE SEPULVEDA <felipe.sc@uc.cl>
2024-09-21 18:48:30 -0300

    Configuracion de Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Partidos from "./pages/partidos/partidos.jsx";
 import DetallePartidoPage from "./pages/DetallePartido/DetallePartidoPage.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
+
   return (
     <Router>
       <Routes>
         <Route path="/partidos" element={<Partidos />} />
         <Route path="/partido/:fixtureId" element={<DetallePartidoPage />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
