60b66d6578f3795f7f439e7353e451a50b9b3e6b
mojojitos <majobarra.2000@uc.cl>
2024-09-28 20:00:45 -0300

    notificacion pop up defined

--- src/components/Notificacion/Notificacion.jsx
+++ src/components/Notificacion/Notificacion.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Notificacion.scss";
+import axios from "axios";
 
 function Notificacion() {
+    const [mostrarNotificacion, setMostrarNotificacion] = useState(false);
+    const [mensaje, setMensaje] = useState("");
+
+    useEffect(() => {
+        axios.get("http://api-g11:3000/showNotifications/:id_user")
+        .then(response => {
+            if (response.data.show) {
+                setMensaje(response.data.message);
+                setMostrarNotificacion(true);
+            }
+        })
+        .catch(error => {
+            console.error("Error al obtener el mensaje de la notificación", error);
+        });
+    },[]);
+
     return (
-        <div class="notificacion is-info">
-            <button class="delete"></button>
-            ¡Partido Ganado! Has obtenido XXX creditos.
-        </div>
+        mostrarNotificacion && (
+            <div class="notificacion is-info">
+                <button class="delete" onClick={() => setMostrarNotificacion(false)}></button>
+                {mensaje}
+            </div>
+        )
     );
 };
 
