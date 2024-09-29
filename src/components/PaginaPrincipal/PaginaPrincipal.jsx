4da4d637c0efd7b29955de370057797e6f296bb2
mojojitos <majobarra.2000@uc.cl>
2024-09-29 03:38:44 -0300

    Aesthetic changes pagina principal

--- src/components/PaginaPrincipal/PaginaPrincipal.jsx
+++ src/components/PaginaPrincipal/PaginaPrincipal.jsx
@@ -5,17 +5,25 @@ import { Link } from 'react-router-dom';
 const PaginaPrincipal = () => {
     return (
         <div class="pagina-principal">
-            <h1 class="title is-1">¡Bienvenido a CoolGoat!</h1>
-            <div class="columns">
-                <div class="column">
-                    <Link class="hero is-large is-info" to="/partidos">
-                        <div class="hero-body">
-                            <h3 class="title is-3">Partidos</h3>
-                            <h5 class="subtitle is-5">Revisa los partidos que estan en desarrollo</h5>
+            <div class="header-section has-background-black-ter">
+                <div class="columns is-gapless">
+                    <div class="column is-one-quarter">
+                        <h1 class="title is-1">Bienvenido a</h1>
+                    </div>
+                    <div class="column is-half">
+                        <div class="header-icon">
+                            <img src="/icon.png" alt="Logo" class="logo" />
                         </div>
-                    </Link>
+                    </div>
                 </div>
+                <p class="subtitle is-3 has-text-centered">¡La mejor plataforma de apuestas deportivas!</p>
             </div>
+            <Link class="hero is-small is-info" to="/partidos">
+                <div class="hero-body">
+                    <h3 class="title is-3">Partidos en juego</h3>
+                    <h5 class="subtitle is-5">¡Haz click aqui para ver los partidos que estan en desarrollo!</h5>
+                </div>
+            </Link>
         </div>
     );
 };
