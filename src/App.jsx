0302934faa0785e711ac633cf402895232265a5e
mojojitos <majobarra.2000@uc.cl>
2024-09-29 20:44:43 -0300

    conflict fix 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Partidos from "./components/Partidos/Partidos.jsx";
+import Partidos from "./components/Partidos/Partido.jsx";
 import DetallePartidoPage from "./components/DetallePartido/DetallePartidoPage.jsx";
 import Login from './components/Login/Login.jsx';
 import PaginaPrincipal from './components/PaginaPrincipal/PaginaPrincipal.jsx';
