fee2f93ad79c5d1ef9123ba7167ef59107b93b8b
FELIPE SEPULVEDA <felipe.sc@uc.cl>
2024-09-29 21:03:37 -0300

    Recorrecion rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Partidos from "./components/Partidos/Partido.jsx";
+import Partidos from "./components/Partidos/PartidosPage.jsx";
 import DetallePartidoPage from "./components/DetallePartido/DetallePartidoPage.jsx";
 import Login from './components/Login/Login.jsx';
 import PaginaPrincipal from './components/PaginaPrincipal/PaginaPrincipal.jsx';
@@ -10,7 +10,8 @@ import Signup from './components/Signup/Signup.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import HistorialNotificacion from './components/HistorialNotificacion/HistorialNotificacion.jsx';
 import HistorialCompra from './components/HistorialCompra/HistorialCompra.jsx';
-
+import DetallePartidoTerminado from './components/PartidosTerminados/DetallePartidoTerminado.jsx';
+import PartidosTerminados from './components/PartidosTerminados/PartidosTerminados.jsx';
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
         <Route path="/pagina-principal" element={<PaginaPrincipal />} />
         <Route path="/partidos" element={<Partidos />} />
         <Route path="/partido/:fixtureId" element={<DetallePartidoPage />} />
+        <Route path="/partido-terminado/:fixtureId" element={<DetallePartidoTerminado />} />
+        <Route path="/resultados" element={<PartidosTerminados />} />
         <Route path="/wallet" element={<Wallet />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
