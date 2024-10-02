e257ec5197163f94fb4e034f4cbcf4841362707a
mojojitos <majobarra.2000@uc.cl>
2024-10-02 06:24:34 -0300

    hist compras

--- src/components/HistorialCompra/HistorialCompra.jsx
+++ src/components/HistorialCompra/HistorialCompra.jsx
@@ -58,10 +58,17 @@ function HistorialCompra() {
                 <ul>
                     {comprasxPagina.map((compra, index) => (
                         <li key={index} className="transaccion">
-                            <div className="hero is-small has-background-grey-darker">
-                                <div className="hero-body">
-                                    <p className="title"> Fecha de transaccion: {Date(compra.date_transaction).toLocaleString()} </p>
-                                    <p className="subtitle"> Monto de compra: {compra.cost_transaction} </p>
+                            <div className="card-content">
+                                <div className="media-content">
+                                    <p className="title is-3"> Bono comprado para {compra.home} v/s {compra.away} </p>
+                                    <p className="subtitle is-4"> Valor de la transaccion: ${compra.cost_transaction} </p>
+                                    <div className="content">
+                                    <p className="subtitle is-4"> 
+                                        {compra.money_victory > 0 && "Dinero ganado: $" + compra.money_victory}
+                                        {compra.money_defeat > 0 && "Dinero perdido: $" + compra.money_defeat}
+                                    </p>
+                                    <p className="subtitle is-6"> Fecha de la transaccion: {new Date(compra.date_transaction).toLocaleString()} </p>
+                                    </div>
                                 </div>
                             </div>
                         </li>
