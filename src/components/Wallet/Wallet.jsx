cd4c14ad01b83efebf4a472d2813315e53efe778
mojojitos <majobarra.2000@uc.cl>
2024-10-02 03:42:48 -0300

    url change

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Wallet.scss';
-import { jwtDecode } from "jwt-decode";
+// import { jwtDecode } from "jwt-decode";
 
 const Wallet = () => {
     const [recarga, setRecarga] = useState(0); // Monto De Recarga
     const [monto, setMonto] = useState(''); // Monto Actual
+    const [UserId, setUserId] = useState(null);
 
     useEffect(() => {
-        const token = localStorage.getItem('accessToken');
-        if (token) {
-            const decodedToken = jwtDecode(token);
-            const id_user = decodedToken.sub;
-        axios.get(`https://grupo11backend.me/getWalletCredit/${id_user}`) // Mostrar dinero actual
+        // const token = localStorage.getItem('accessToken');
+        // if (token) {
+            // const decodedToken = jwtDecode(token);
+            // const id_user = decodedToken.sub;
+        const storedUserId = localStorage.getItem("user");
+        if (storedUserId) {
+            setUserId(storedUserId);
+        };
+
+        axios.get(`https://grupo11backend.me/getWalletCredit/${UserId}`) // Mostrar dinero actual
             .then(response => 
                 {setMonto(response.data.monto);
                 })
             .catch(error => 
                 {console.error('Error al cargar tu monto actual:', error)
                 });
-            }
     }, []);
 
     const RecargarCreditos = (event) => {  // Recarga de creditos
