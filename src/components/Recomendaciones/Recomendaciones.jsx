1472b700c92c46255ebd6e6a6f9e542147baa2d0
FELIPE SEPULVEDA <felipe.sc@uc.cl>
2024-10-30 23:53:49 -0300

    arreglar css

--- src/components/Recomendaciones/Recomendaciones.jsx
+++ src/components/Recomendaciones/Recomendaciones.jsx
@@ -28,32 +28,32 @@ const Recomendaciones = () => {
       }, [userId]);
 
   return (
-    <div className="recomendaciones-list">
+    <div className="recomendaciones-container">
       <h1>Partidos Recomendados</h1>
-      <ul>
+      <ul className="recomendaciones-list">
         {recomendaciones.map((recomendacion) => (
-          <li key={recomendacion.fixture_id}>
+          <li key={recomendacion.fixture_id} className="recomendaciones-item">
             <Link to={`/partido/${recomendacion.fixture_id}`}>
-              <div className="match-info">
-                <div className="team-logo">
+              <div className="recomendaciones-match-info">
+                <div className="recomendaciones-team-logo">
                   <img
                     src={recomendacion.teams.home.logo}
                     alt={`${recomendacion.teams.home.name} logo`}
                   />
                 </div>
-                <div className="team-name">{recomendacion.teams.home.name}</div>
+                <div className="recomendaciones-team-name">{recomendacion.teams.home.name}</div>
                 <span>vs</span>
-                <div className="team-name">{recomendacion.teams.away.name}</div>
-                <div className="team-logo">
+                <div className="recomendaciones-team-name">{recomendacion.teams.away.name}</div>
+                <div className="recomendaciones-team-logo">
                   <img
                     src={recomendacion.teams.away.logo}
                     alt={`${recomendacion.teams.away.name} logo`}
                   />
                 </div>
-                <div className="match-date">
+                <div className="recomendaciones-match-date">
                   {new Date(recomendacion.date).toLocaleString()}
                 </div>
-                <div className="league-logo">
+                <div className="recomendaciones-league-logo">
                   <img src={recomendacion.league.logo} alt="Logo de la liga" />
                 </div>
               </div>
